6e7ea2f8dc0eb8608718cf0063cda497abb2df9b
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-02-07 13:49:55 +0500

    Cors error fixed

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.status(200).json({});
+  }
+  next();
+});
 
 app.use((req, res, next) => {
   const error = new Error("Not found this route");
