78e214b69f9e3af76ce620e917a1c2de9af40b3d
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-02-07 12:30:57 +0500

    project upgraded

--- server/app.js
+++ server/app.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  const error = new Error("Not found this route");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: error.message,
+  });
+});
 module.exports = app;
